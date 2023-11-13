567acd1a6d0d1f1a59c64aea68a9ebe7d551b518
Mindy D Tran <mindytran98@gmail.com>
2023-11-13 16:43:12 -0500

    added console.log after some of the validation test

--- script.js
+++ script.js
@@ -9,29 +9,24 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
-
+console.log(`Is the sum 50?: ${isSum50}`);
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
-
+console.log(`Are there at least 2 odd numbers: ${isTwoOdd}`);
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-
+console.log(`Is there numbers over 25? ${isOver25}`);
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
-
-// Here, we put the results into a single variable 
-// for convenience. Note how we negate isOver25 using
-// the ! operator. We could also have tested for 
-// "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+console.log(`Are all number unique? ${isUnique}`);
 
 // Start of Part 1
 // Check if all number is divisible by 5
@@ -42,10 +37,18 @@ const isDivisibleBy5 = (n1%5) + (n2%5) + (n3%5) + (n4%5) === 0 ;
 // check if the first number is larger than the last number
 const is1stLargerThanLast = n1 > n4;
 
+// self explainatory.
+const arithmeticChain = (n2-n1) * n3 % n4;
 // end of part 1
 
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+
 // Finally, log the results.
-console.log(isValid);
+console.log(`Did it meet all of the conditions? ${isValid}`);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
